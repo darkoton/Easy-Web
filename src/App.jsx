35b7446826dd3355abe5699a9e1d631b3e907c73
Artem <darkoto12@gmail.com>
2024-08-09 12:15:39 +0300

    delete router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import '@/assets/style/style.css';
 import Footer from '@/components/Footer/Footer';
 import { RouterProvider } from 'react-router-dom';
-import router from '@/router';
+import Home from '@/pages/Home';
 
 function App() {
   return (
     <div className="wrapper w-full ">
       {/* Header */}
       <main className="content">
-        <RouterProvider router={router()} />
+        <Home />
       </main>
       <Footer />
     </div>
